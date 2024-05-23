03b563ac954a668295bab52857962f3c221dd1c1
mar1pac <nightshadiw2@gmail.com>
2024-05-23 03:11:45 +0000

    login

---
+++ add_product.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace C_S
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Desktop;
+            label1.Location = new Point(139, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(198, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Add Product :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(139, 138);
+            label2.Name = "label2";
+            label2.Size = new Size(179, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Product Name";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(398, 211);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(232, 33);
+            comboBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(139, 208);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 32);
+            label3.TabIndex = 3;
+            label3.Text = "Type";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(139, 285);
+            label4.Name = "label4";
+            label4.Size = new Size(113, 32);
+            label4.TabIndex = 4;
+            label4.Text = "Quantity";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(596, 369);
+            button1.Name = "button1";
+            button1.Size = new Size(128, 52);
+            button1.TabIndex = 5;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(398, 141);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(232, 31);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(398, 288);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(232, 31);
+            textBox2.TabIndex = 7;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
